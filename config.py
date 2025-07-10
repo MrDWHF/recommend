8a9652bea198139bdc26a401066658333ee32481
MrDWHF <1370781159@qq.com>
2025-07-10 16:07:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,13 @@
-# config.py
-
-# DB_CONFIG = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '123456',
-#     'database': 'finalbooks',
-#     'charset': 'utf8mb4'
-# }
-
 DB_CONFIG = {
-    #'host': '10.242.22.38',        # 主机地址
-    'host': '10.242.8.100',        # 主机地址
-    'user': 'root',                # 用户名
-    'password': 'mysql', # 密码
-    'database': 'kaotu',                # 数据库名（请填写实际的数据库名）
+    #'host': '',        # 主机地址
+    'host': '',        # 主机地址
+    'user': '',                # 用户名
+    'password': 'l', # 密码
+    'database': '',                # 数据库名（请填写实际的数据库名）
     'charset': 'utf8mb4',          # 字符集
-    'port': 16728                  # 端口号
+    'port':                   # 端口号
 }
 
 
-DEEPSEEK_API_KEY='sk-67a23795b8eb4884bd0c1abba7e9bc48'
+DEEPSEEK_API_KEY='你的api key'
 DEEPSEEK_API_URL='https://api.deepseek.com'
